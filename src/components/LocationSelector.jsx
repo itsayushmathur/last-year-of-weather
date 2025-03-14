0e8d899f65aa949146c1a8bfb7a982057b442fbc
Ayush Mathur <ayush.mathur@incture.com>
2025-03-14 11:25:21 +0530

    removed the logic to open the dropdown only when user typed inside it.

--- src/components/LocationSelector.jsx
+++ src/components/LocationSelector.jsx
@@ -3,22 +3,18 @@ import { Autocomplete, TextField, Box } from "@mui/material";
 
 const LocationSelector = ({ locations, onSelect }) => {
   const [inputValue, setInputValue] = useState("");
-  const [open, setOpen] = useState(false);
 
-  //will open the dropdown only the user types something
+  //removed the logic to open the dropdown only when user typed inside it. 
   const handleInputChange = (event, newInputValue) => {
     setInputValue(newInputValue);
-    setOpen(newInputValue.length > 0);
   };
 
   return (
-    <Box sx={{width: "100%" }}>
+    <Box sx={{ width: "100%" }}>
       <Autocomplete
         options={locations}
         inputValue={inputValue}
-        open={open}
         onInputChange={handleInputChange}
-        onClose={() => setOpen(false)}
         onChange={(event, newValue) => onSelect(newValue)}
         renderInput={(params) => (
           <TextField {...params} label="City" variant="outlined" />
